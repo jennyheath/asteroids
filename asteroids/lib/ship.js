f64b59abb4a31be3d18919dbf751b0078675082a
App Academy Student <student@appacademy.io>
2015-05-25 20:09:43 -0700

    Make bullet moving objects

--- asteroids/lib/ship.js
+++ asteroids/lib/ship.js
@@ -11,9 +11,16 @@
   };
   Asteroids.Util.inherits(Ship, Asteroids.MovingObject);
 
+  Ship.prototype.fireBullet = function () {
+    // if (this.game.bullets.length < 5) {
+      var bullet = new Asteroids.Bullet(this.game, this.pos, this.vel);
+      this.game.bullets.push(bullet);
+    // }
+  };
+
   Ship.prototype.power = function (impulse) {
     this.vel[0] += impulse[0];
-    this.vel[1] += impulse[1];    
+    this.vel[1] += impulse[1];
   };
 
   Ship.prototype.relocate = function () {
